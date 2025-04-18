7590a12430ad96bd7d3e5b61bef1d177f05f835a
Han Lin Mai <h.mai@mpie.de>
2025-04-18 15:49:44 +0200

    python package

---
+++ pyiron_workflow_assyst/__init__.py
@@ -0,0 +1,8 @@
+"""
+pyiron_workflow_assyst - A workflow package for ASSYST (Automated Symmetry and Stability Testing)
+"""
+
+from .workflow import *
+from .structure_filter_utils import *
+
+__version__ = "0.1.0" 
