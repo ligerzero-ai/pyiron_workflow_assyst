0a7a72d17c16e3b00ce5f24dd1d5d0dec92abe9d
Han Lin Mai <h.mai@mpie.de>
2025-05-01 17:25:09 +0200

    get rid of bug where node can't have empty dict or same name or somehow non used arg - serialisation error in pyiron_workflow

--- pyiron_workflow_assyst/workflow.py
+++ pyiron_workflow_assyst/workflow.py
@@ -352,9 +352,9 @@ def run_ASSYST_on_structure(
     remove_calc_dirs=True,
     train_df_filename="df_ASSYST_jobs.pkl",
     vasp_parser_function=parse_vasp_directory,
-    vasp_parser_args={},
+    #vasp_parser_args={},
 ):
-    wf.base_structure = convert_pymatgen_to_ase(structure)
+    wf.base_structure = structure #convert_pymatgen_to_ase(structure)
     wf.ISIF_7_modded_ionicsteps_dict = get_ionic_steps_dict(incar, ionic_steps=ionic_steps)
    
     wf.ISIF7_incar = generate_modified_incar(wf.ISIF_7_modded_ionicsteps_dict , {"ISIF": 7})
